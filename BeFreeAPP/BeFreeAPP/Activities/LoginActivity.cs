d59d30ebff123f850256bc76fabaa7b723cff86f
Guilherme Domingues <guilhermes.domingues@gmail.com>
2017-05-07 13:06:39 -0300

    commit

--- BeFreeAPP/BeFreeAPP/Activities/LoginActivity.cs
+++ BeFreeAPP/BeFreeAPP/Activities/LoginActivity.cs
@@ -38,24 +38,36 @@ namespace BeFreeAPP.Activities
             EditText edtUsuario = FindViewById<EditText>(Resource.Id.edtUsuario);
             EditText edtSenha = FindViewById<EditText>(Resource.Id.edtSenha);
             List<Usuario> usuarios = null;
-            using (HttpClient client = new HttpClient())
+
+            try
             {
-                ;
-                Uri uri = new Uri("http://192.168.219.57:8080/api/Usuarios/");
-                var response = await client.GetAsync(uri);
-                if (response.IsSuccessStatusCode)
+                using (HttpClient client = new HttpClient())
                 {
-                    var result = response.Content.ReadAsStringAsync().Result;
-                    var _clsUsuario = JsonConvert.DeserializeObject(result);
-                    usuarios = JsonConvert.DeserializeObject<List<Usuario>>(_clsUsuario.ToString());
+                    Uri uri = new Uri("http://192.168.1.36:8080/api/Usuarios/");
+                    var response = await client.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = response.Content.ReadAsStringAsync().Result;
+                        var _clsUsuario = JsonConvert.DeserializeObject(result);
+                        usuarios = JsonConvert.DeserializeObject<List<Usuario>>(_clsUsuario.ToString());
 
-                    if (usuarios.Where(u => u.nomeUsuario == edtUsuario.Text).Count() > 0) {
+                        if (usuarios.Where(u => u.nomeUsuario == edtUsuario.Text && u.senha == edtSenha.Text).Count() > 0)
+                        {
 
-
+                            Toast.MakeText(this, "Login realizado com sucesso", ToastLength.Short).Show();
+                        }
+                        else
+                        {
+                            Toast.MakeText(this, "Nome do Usuário e/ou Senha invalida(os)", ToastLength.Short).Show();
+                        }
                     }
+
                 }
             }
-
+            catch (Exception err)
+            {
+                string erro = err.Message;
+            }
         }
     }
 }
