189d1fb5f38305a6ea76271faab586d1b41b9a8f
Guilherme Domingues <guilhermes.domingues@gmail.com>
2017-05-09 19:58:48 -0300

    Guilherme

--- BeFreeAPP/BeFreeAPP/Resources/Resource.Designer.cs
+++ BeFreeAPP/BeFreeAPP/Resources/Resource.Designer.cs
@@ -78,8 +78,8 @@ namespace BeFreeAPP
 			// aapt resource value: 0x7f050001
 			public const int absoluteLayout1 = 2131034113;
 			
-			// aapt resource value: 0x7f05001b
-			public const int bairro_row_txt = 2131034139;
+			// aapt resource value: 0x7f050020
+			public const int bairro_row_txt = 2131034144;
 			
 			// aapt resource value: 0x7f050009
 			public const int btnCreateAccount = 2131034121;
@@ -90,8 +90,8 @@ namespace BeFreeAPP
 			// aapt resource value: 0x7f050006
 			public const int btnLogin = 2131034118;
 			
-			// aapt resource value: 0x7f05001a
-			public const int cidade_row_txt = 2131034138;
+			// aapt resource value: 0x7f05001f
+			public const int cidade_row_txt = 2131034143;
 			
 			// aapt resource value: 0x7f050005
 			public const int edtSenha = 2131034117;
@@ -102,44 +102,59 @@ namespace BeFreeAPP
 			// aapt resource value: 0x7f05000e
 			public const int linearLayout1 = 2131034126;
 			
-			// aapt resource value: 0x7f050013
-			public const int linearLayout2 = 2131034131;
+			// aapt resource value: 0x7f050018
+			public const int linearLayout2 = 2131034136;
 			
-			// aapt resource value: 0x7f050015
-			public const int linearLayout3 = 2131034133;
+			// aapt resource value: 0x7f05001a
+			public const int linearLayout3 = 2131034138;
 			
 			// aapt resource value: 0x7f05000a
 			public const int progressBar1 = 2131034122;
 			
-			// aapt resource value: 0x7f050012
-			public const int service_anuncio_bairro = 2131034130;
-			
 			// aapt resource value: 0x7f050016
-			public const int service_anuncio_button_comprar = 2131034134;
+			public const int service_anuncio_bairro = 2131034134;
 			
-			// aapt resource value: 0x7f050017
-			public const int service_anuncio_button_denunciar = 2131034135;
-			
-			// aapt resource value: 0x7f050018
-			public const int service_anuncio_button_voltar = 2131034136;
-			
-			// aapt resource value: 0x7f050010
-			public const int service_anuncio_categoria = 2131034128;
-			
-			// aapt resource value: 0x7f050011
-			public const int service_anuncio_cidade = 2131034129;
-			
-			// aapt resource value: 0x7f050014
-			public const int service_anuncio_descricao = 2131034132;
-			
-			// aapt resource value: 0x7f05000f
-			public const int service_anuncio_titulo = 2131034127;
-			
-			// aapt resource value: 0x7f050019
-			public const int servico_row_txt = 2131034137;
+			// aapt resource value: 0x7f05001b
+			public const int service_anuncio_button_comprar = 2131034139;
 			
 			// aapt resource value: 0x7f05001c
-			public const int subcategoria_row_txt = 2131034140;
+			public const int service_anuncio_button_denunciar = 2131034140;
+			
+			// aapt resource value: 0x7f05001d
+			public const int service_anuncio_button_voltar = 2131034141;
+			
+			// aapt resource value: 0x7f050012
+			public const int service_anuncio_categoria = 2131034130;
+			
+			// aapt resource value: 0x7f050014
+			public const int service_anuncio_cidade = 2131034132;
+			
+			// aapt resource value: 0x7f050019
+			public const int service_anuncio_descricao = 2131034137;
+			
+			// aapt resource value: 0x7f050010
+			public const int service_anuncio_titulo = 2131034128;
+			
+			// aapt resource value: 0x7f05001e
+			public const int servico_row_txt = 2131034142;
+			
+			// aapt resource value: 0x7f050021
+			public const int subcategoria_row_txt = 2131034145;
+			
+			// aapt resource value: 0x7f05000f
+			public const int textView1 = 2131034127;
+			
+			// aapt resource value: 0x7f050011
+			public const int textView2 = 2131034129;
+			
+			// aapt resource value: 0x7f050013
+			public const int textView3 = 2131034131;
+			
+			// aapt resource value: 0x7f050015
+			public const int textView4 = 2131034133;
+			
+			// aapt resource value: 0x7f050017
+			public const int textView5 = 2131034135;
 			
 			// aapt resource value: 0x7f050007
 			public const int txtCreateAccount = 2131034119;
