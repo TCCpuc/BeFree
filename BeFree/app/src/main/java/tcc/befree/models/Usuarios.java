6c176df3bc9a3bbdab0d682a0672f522a7d182de
Guilherme Domingues <guilhermes.domingues@gmail.com>
2017-05-17 18:44:25 -0300

    Novo Projeto
    Novo Projeto

---
+++ BeFree/app/src/main/java/tcc/befree/models/Usuarios.java
@@ -0,0 +1,25 @@
+package tcc.befree.models;
+
+import java.util.Date;
+
+/**
+ * Created by Guilherme Domingues on 5/16/2017.
+ */
+
+public class Usuarios {
+    public int idUsuario ;
+    public String nomeUsuario;
+    public String cpf ;
+    public int idCidade ;
+    public int idEstado ;
+    public String bairro ;
+    public String logradouro;
+    public String numero ;
+    public String cep ;
+    public Date dataNascimento;
+    public Date dataCadastro;
+    public boolean ativo;
+    public String senha ;
+    public String email ;
+    public String ddd ;
+}
