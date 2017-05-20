c3140ac95c130c2273d939d4f26cd402d7063315
s0orana <guilherme.leme@MAC-056.local>
2017-05-20 16:07:12 -0300

    Arrumado Layout do Serviço

--- BeFree/app/src/main/java/tcc/befree/SlideActivity.java
+++ BeFree/app/src/main/java/tcc/befree/SlideActivity.java
@@ -1,5 +1,6 @@
 package tcc.befree;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,11 +18,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SlideActivity extends AppCompatActivity {
@@ -129,15 +133,15 @@ public class SlideActivity extends AppCompatActivity {
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
 
 
+
                 ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
-                        R.array.Planets, android.R.layout.simple_list_item_1);
+                    R.array.Planets, android.R.layout.simple_list_item_1);
 
                 ListView ls = (ListView) rootView.findViewById(R.id.list);
                 ls.setAdapter(adapter);
 
 
-
-            System.out.println("MAE EU TE AMO" + getArguments().getInt(ARG_SECTION_NUMBER));
+            System.out.println(R.array.Planets+ "  MAE EU TE AMO " + getArguments().getInt(ARG_SECTION_NUMBER + "   "));
 
 
             return rootView;
