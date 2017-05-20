6351b56ca1463f0ea885e4995e2f7aacc19d97d5
gabro <gabro@DESKTOP-O5CUHKB>
2017-05-20 11:57:26 -0300

    Tela Anuncio

---
+++ BeFree/app/src/main/java/tcc/befree/models/Busca.java
@@ -0,0 +1,17 @@
+package tcc.befree.models;
+
+/**
+ * Created by gabro on 20/05/2017.
+ */
+
+public class Busca {
+    public String nome;
+    public String categoria ;
+    public String subcategoria ;
+    public String cidade ;
+    public String bairro ;
+    public String descricao ;
+    public int id_busca ;
+    public long cpf ;
+    public boolean status ;
+}
---
+++ BeFree/app/src/main/java/tcc/befree/models/Servico.java
@@ -0,0 +1,22 @@
+package tcc.befree.models;
+
+/**
+ * Created by gabro on 20/05/2017.
+ */
+
+public class Servico {
+    //public string nome;
+    //public string categoria ;
+    //public string subcategoria ;
+    //public string cidade ;
+    //public string bairro ;
+    //public string descricao;
+    //public int id_servico;
+    //public long cpf;
+    public int idServico;
+    public String titulo;
+    public String descricao;
+    public int idUsuario;
+    public int idSubCategoria;
+    public int idStatus;
+}
