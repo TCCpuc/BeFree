ba944db21a17e90514d44066e57104c8a9156853
Guilherme Domingues <guilhermes.domingues@gmail.com>
2017-05-23 14:11:51 -0300

    Post API

--- BeFree/app/src/main/java/tcc/befree/AnuncioActivity.java
+++ BeFree/app/src/main/java/tcc/befree/AnuncioActivity.java
@@ -1,8 +1,10 @@
 package tcc.befree;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -24,10 +26,15 @@ public class AnuncioActivity extends AppCompatActivity {
         Servico srv = new Servico();
         srv = conexao.getServicosById(id);
 
-        ((TextView)findViewById(getResources().getIdentifier("txtNome", "id", getPackageName()))).setText(srv.idServico);
-        ((TextView)findViewById(getResources().getIdentifier("txtDescricao", "id", getPackageName()))).setText(srv.descricao);
+        try {
+            ((TextView) findViewById(getResources().getIdentifier("txtNome", "id", getPackageName()))).setText(srv.idServico);
+            ((TextView) findViewById(getResources().getIdentifier("txtDescricao", "id", getPackageName()))).setText(srv.descricao);
 //        ((TextView)findViewById(getResources().getIdentifier("txtCidade", "id", getPackageName()))).setText(conexao.getServicosById(id).);
 //        ((TextView)findViewById(getResources().getIdentifier("txtCategoria", "id", getPackageName()))).setText(conexao.getServicosById(id).);
 //        ((TextView)findViewById(getResources().getIdentifier("txtSubCategoria", "id", getPackageName()))).setText(conexao.getServicosById(id).);
+        }catch (Exception e){
+            String erro = e.getMessage();
+            Toast.makeText(this,erro,Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ BeFree/app/src/main/java/tcc/befree/PostApiModels.java
@@ -0,0 +1,15 @@
+package tcc.befree;
+
+/**
+ * Created by Guilherme Domingues on 5/23/2017.
+ */
+
+public class PostApiModels implements Runnable{
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
