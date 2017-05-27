72c3898e22d8786352d19d02fc30802291e9700f
Guilherme Domingues <guilhermes.domingues@gmail.com>
2017-05-27 13:39:15 -0300

    API POST

--- BeFree/app/src/main/java/tcc/befree/PostApiModels.java
+++ BeFree/app/src/main/java/tcc/befree/PostApiModels.java
@@ -1,11 +1,27 @@
 package tcc.befree;
 
+import tcc.befree.models.*;
+
 /**
  * Created by Guilherme Domingues on 5/23/2017.
  */
 
 public class PostApiModels implements Runnable{
 
+    public boolean postServico(Servico servico){
+
+        return true;
+    }
+
+    public boolean postBusca(Busca busca){
+
+        return true;
+    }
+
+    public boolean postUsuarios(Usuarios usuario){
+
+        return true;
+    }
 
     @Override
     public void run() {
