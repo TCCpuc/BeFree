00d03c4003749af921745d103b46e8d64cefb27f
gabro <gabro@DESKTOP-O5CUHKB>
2017-06-07 20:56:39 -0300

    funcionando (só o ddd)

--- BeFree/app/src/main/java/tcc/befree/CreateBuscaActivity.java
+++ BeFree/app/src/main/java/tcc/befree/CreateBuscaActivity.java
@@ -28,18 +28,18 @@ public class CreateBuscaActivity extends AppCompatActivity {
         arrayAdapterDDD.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
         spinnerDDDs.setAdapter(arrayAdapterDDD);
 
-        //popula o spinner de subcategoria
-        Spinner spinnerSubCategorias = (Spinner) findViewById(R.id.create_busca_spinnerCategoria);
-        String[] vetorSubCategorias = conexao.getSubCategoriasVetor();
-        ArrayAdapter<CharSequence>  arrayAdapterSubCategoria = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, vetorSubCategorias);
-        arrayAdapterSubCategoria.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
-        spinnerSubCategorias.setAdapter(arrayAdapterSubCategoria);
-
-        //popula o spinner de categoria
-        Spinner spinnerCategorias = (Spinner) findViewById(R.id.create_busca_spinnerSubCategoria);
-        String[] vetorCategorias = conexao.getCategoriasVetor();
-        ArrayAdapter<CharSequence>  arrayAdapterCategoria = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, vetorCategorias);
-        arrayAdapterCategoria.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
-        spinnerCategorias.setAdapter(arrayAdapterCategoria);
+//        //popula o spinner de subcategoria
+//        Spinner spinnerSubCategorias = (Spinner) findViewById(R.id.create_busca_spinnerCategoria);
+//        String[] vetorSubCategorias = conexao.getSubCategoriasVetor();
+//        ArrayAdapter<CharSequence>  arrayAdapterSubCategoria = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, vetorSubCategorias);
+//        arrayAdapterSubCategoria.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+//        spinnerSubCategorias.setAdapter(arrayAdapterSubCategoria);
+//
+//        //popula o spinner de categoria
+//        Spinner spinnerCategorias = (Spinner) findViewById(R.id.create_busca_spinnerSubCategoria);
+//        String[] vetorCategorias = conexao.getCategoriasVetor();
+//        ArrayAdapter<CharSequence>  arrayAdapterCategoria = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, vetorCategorias);
+//        arrayAdapterCategoria.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+//        spinnerCategorias.setAdapter(arrayAdapterCategoria);
     }
 }
