4c116e3d2a1127dc8aaeb6bb95552d3cf741df6c
Guilherme Domingues <guilhermes.domingues@gmail.com>
2017-08-20 17:20:17 -0300

    Facebook Login
    Commit com a alteracao do botao do facebook e login funcionando

--- BeFree/app/src/main/java/tcc/befree/activities/LoginActivity.java
+++ BeFree/app/src/main/java/tcc/befree/activities/LoginActivity.java
@@ -10,6 +10,16 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 import tcc.befree.api.ApiModels;
 
 import java.util.ArrayList;
@@ -26,23 +36,52 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextInputEditText Email = null;
     private TextInputEditText Password = null;
-
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private AccessToken accessToken;
     private View mProgressView;
     
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
 
-        /*
-        This activity is created and opened when SplashScreen finishes its animations.
-        To ensure a smooth transition between activities, the activity creation animation
-        is removed.
-        RelativeLayout with EditTexts and Button is animated with a default fade in.
-         */
+        loginButton = (LoginButton) findViewById(R.id.login_facebook_button);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
 
-        //mProgressView.setVisibility(View.GONE);
+
+                //txtStatus.setText("Login Sucess \n" + loginResult.getAccessToken().getUserId() + "\n" + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                //txtStatus.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+
+            }
+        };
+
+
+
+        // If the access token is available already assign it.
+        accessToken = AccessToken.getCurrentAccessToken();
 
         overridePendingTransition(0,0);
         View relativeLayout=findViewById(R.id.newLogin_container);
