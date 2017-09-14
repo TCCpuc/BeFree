b3961495d59503dfb65f1a912f1fa87d6f50575f
s0orana <guilherme_leme@yahoo.com.br>
2017-09-14 17:54:56 -0300

    Forgot password interface OK
    Forgot password interface OK

--- BeFree/app/src/main/java/tcc/befree/telas/Dialog/ForgotPasswordDialog.java
+++ BeFree/app/src/main/java/tcc/befree/telas/Dialog/ForgotPasswordDialog.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import tcc.befree.R;
 
@@ -16,8 +17,14 @@ import tcc.befree.R;
 public class ForgotPasswordDialog extends Dialog implements
         android.view.View.OnClickListener{
 
-    public Activity c;
-    public Dialog d;
+    private Activity c;
+    private Button sendButton;
+    private Button backButton;
+    private TextView textMessage;
+    private EditText dialogMessage;
+    private EditText passwordText;
+    private EditText passwordText2;
+    private int count = 0;
 
     public ForgotPasswordDialog(Activity a) {
         super(a);
@@ -30,23 +37,64 @@ public class ForgotPasswordDialog extends Dialog implements
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.forgot_password_dialog);
-        Button dialogButton = (Button) findViewById(R.id.dialog_btn_send);
-        dialogButton.setOnClickListener(this);
+        sendButton = (Button) findViewById(R.id.dialog_btn_send);
+        backButton = (Button) findViewById(R.id.dialog_btn_back);
+        textMessage = (TextView) findViewById(R.id.dialog_text);
+        dialogMessage = (EditText) findViewById(R.id.dialog_message);
+        passwordText = (EditText) findViewById(R.id.dialog_password);
+        passwordText2 = (EditText) findViewById(R.id.dialog_password2);
+        sendButton.setOnClickListener(this);
+        backButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.dialog_btn_send:
-                c.finish();
+                if(count == 0){
+                    //VERIFICAR NO BANCO SE EMAIL EXISTE
+                    if(dialogMessage.getText().toString() != ""){
+                        textMessage.setText("Digite o codigo enviado para seu email");
+                        dialogMessage.setText("");
+                        dialogMessage.setHint("QF94fg");
+                        count ++;
+                    }else
+                        dialogMessage.setText("");
+                        textMessage.setText("Email nao encontrado, digite novamente");
+                }else if(count == 1){
+                    //VERIFICAR NO BANCO SE CODIGO É CORRETO
+                    if(dialogMessage.getText().toString() != ""){
+                        textMessage.setText("Digite uma nova senha");
+                        dialogMessage.setText("");
+                        dialogMessage.setVisibility(View.GONE);
+                        backButton.setVisibility(View.GONE);
+                        passwordText.setVisibility(View.VISIBLE);
+                        passwordText2.setVisibility(View.VISIBLE);
+                        count ++;
+                    }else
+                        dialogMessage.setText("");
+                        textMessage.setText("Codigo invalido, digite novamente");
+                }else if (count == 2){
+                    if (passwordText.getText() == passwordText2.getText()){
+                        textMessage.setText("Senha alterada com sucesso");
+                        passwordText.setVisibility(View.GONE);
+                        passwordText2.setVisibility(View.GONE);
+                        count = 0;
+                    }else
+                        passwordText.setText("");
+                        passwordText2.setText("");
+                        textMessage.setText("Senhas diferentes, tente novamente");
+                }else
+                    textMessage.setText("ERRO");
                 break;
             case R.id.dialog_btn_back:
+                c.finish();
                 dismiss();
                 break;
             default:
                 break;
         }
-        dismiss();
+        //dismiss();
     }
 
 
