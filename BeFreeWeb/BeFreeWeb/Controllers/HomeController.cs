fbc6acc19fb303cfac4f3ae70f5084467f2080e7
VictorFurlan <victor.buschfurlan@gmail.com>
2017-10-17 18:55:26 -0200

    Pagina Anuncios
    Erro carregar imagem admin

--- BeFreeWeb/BeFreeWeb/Controllers/HomeController.cs
+++ BeFreeWeb/BeFreeWeb/Controllers/HomeController.cs
@@ -52,6 +52,11 @@ namespace BeFreeWeb.Controllers
             return View();
         }
 
+        public ActionResult Adverts()
+        {
+            return View();
+        }
+
         // GET: Emitente/Details/5
         public async Task<ActionResult> EfetuarLogin(Usuario usuario)
         {
